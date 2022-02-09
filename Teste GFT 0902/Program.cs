e4cc254a837ffa7f9aa20bada4e35eb1825cec4e
ADRIANA SANTANA <adriana.px.santana@gmail.com>
2022-02-09 12:02:14 -0300

    Teste GFT 0902
    Teste GFT 0902

---
+++ Teste GFT 0902/Program.cs
@@ -0,0 +1,38 @@
+﻿
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+
+        int soma = (a + b + c + d );
+
+
+        Console.WriteLine("O total da soma é : " + soma);
+
+         while (a = 1 , b = 2 , c = 3, d = 4 ; z = 26);
+        Console.WriteLine(z);
+        a++;
+
+    
+       
+
+        }
+
+
+
+        
+
+        
+
+        
+
+
+
+
+
+     
+    
+
+    
+    
+
